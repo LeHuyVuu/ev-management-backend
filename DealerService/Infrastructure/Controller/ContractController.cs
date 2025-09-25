4af5c846a6375d2ddea46a8a222e5b4884e3ed82
world-source-code24 <sangnhse182627@fpt.edu.vn>
2025-09-25 14:57:09 +0700

    Create ContractController.cs

---
+++ DealerService/Infrastructure/Controller/ContractController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductService.DTOs;
+using ProductService.Infrastructure.Services;
+
+namespace ProductService.Infrastructure.Controller;
+
+[ApiController]
+public class ContractController : ControllerBase
+{
+    private readonly ContractService _contractService;
+
+    public ContractController(ContractService contractService)
+    {
+        _contractService = contractService;
+    }
+
+    [HttpGet]
+    [Route("/customers/{customerId}/contracts")]
+    public async Task<IEnumerable<ContractCustomerResponse>> GetAllContractsByCustomerId(Guid customerId)
+    {
+        return await _contractService.GetAllContractsByCustomerId(customerId);
+    }
+}
