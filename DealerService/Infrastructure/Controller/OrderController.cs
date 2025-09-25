4d087657308001d2200d933a5e79a11564c968df
world-source-code24 <sangnhse182627@fpt.edu.vn>
2025-09-25 14:05:29 +0700

    Create OrderController.cs

---
+++ DealerService/Infrastructure/Controller/OrderController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ProductService.DTOs;
+using ProductService.Infrastructure.Services;
+
+namespace ProductService.Infrastructure.Controller;
+
+public class OrderController : ControllerBase
+{
+    private readonly OrderService _orderService;
+
+    public OrderController(OrderService orderService)
+    {
+        _orderService = orderService;
+    }
+    
+    // [Authorize(Roles = "dealer_staff")]
+    [HttpGet]
+    [Route("customers/{id}/orders")]
+    public async Task<IEnumerable<OrderCustomerResponse>> GetAllOrdersByCustomerId(Guid customerId)
+    {
+        return await _orderService.GetAllOrdersByCustomerId(customerId);
+    }
+}
