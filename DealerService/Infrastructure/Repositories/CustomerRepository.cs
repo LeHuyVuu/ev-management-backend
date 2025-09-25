7953cc7ca99ad4327f3e6394450eaa6978a4f550
world-source-code24 <sangnhse182627@fpt.edu.vn>
2025-09-25 09:20:05 +0700

    Update Customer Repository
    Add GetAllCustomers() Function

--- DealerService/Infrastructure/Repositories/CustomerRepository.cs
+++ DealerService/Infrastructure/Repositories/CustomerRepository.cs
@@ -1,6 +1,19 @@
-﻿namespace ProductService.Infrastructure.Repositories;
+﻿using Microsoft.EntityFrameworkCore;
+using ProductService.Context;
+using ProductService.Entities;
+
+namespace ProductService.Infrastructure.Repositories;
 
 public class CustomerRepository
 {
-    
+    private readonly MyDbContext _dbContext;
+
+    public CustomerRepository(MyDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    public async Task<IEnumerable<Customer>> GetAllCustomers()
+    {
+        return await _dbContext.Customers.ToListAsync();
+    }
 }
