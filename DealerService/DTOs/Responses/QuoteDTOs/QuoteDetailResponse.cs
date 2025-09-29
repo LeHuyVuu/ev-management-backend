4a3eef28453f68b0d65351c0e35e497decea5bbc
world-source-code24 <sangnhse182627@fpt.edu.vn>
2025-09-29 17:15:37 +0700

    Create QuoteResponse and QuoteRepository

---
+++ DealerService/DTOs/Responses/QuoteDTOs/QuoteDetailResponse.cs
@@ -0,0 +1,27 @@
+﻿namespace ProductService.DTOs.Responses.QuoteDTOs;
+
+public class QuoteDetailResponse
+{
+    public Guid QuoteId {get; set;}
+    
+    public string CustomerName {get; set;}
+    
+    public string CustomerPhone {get; set;}
+    
+    public string VehicleBrand {get; set;}
+    
+    public string VehicleModelName {get; set;}
+    
+    public string VehicleVersionName {get; set;}
+    
+    public string VersionColor {get; set;}
+    
+    public string PromotionName {get; set;}
+    
+    public decimal WholesalePrice1 { get; set; }
+    
+    public decimal DiscountRate { get; set; }
+
+    public decimal FinalPrice => WholesalePrice1 - (WholesalePrice1 * DiscountRate);
+
+}
--- DealerService/Infrastructure/Repositories/QuoteRepository.cs
+++ DealerService/Infrastructure/Repositories/QuoteRepository.cs
@@ -10,4 +10,6 @@ public class QuoteRepository
     {
         _dbContext = dbContext;
     }
+    
+    
 }
