0c23b0041ae82c61d4e1dcc30aedaddc58ee83ea
Tran Kim Nha <trankimnha272727@gmail.com>
2025-10-02 23:54:30 +0700

    delete old code

--- DealerService/DTOs/Responses/DealerTargetDTOs/DealerTargetResponse.cs
+++ DealerService/DTOs/Responses/DealerTargetDTOs/DealerTargetResponse.cs
@@ -4,6 +4,7 @@
     {
         public Guid DealerTargetId { get; set; }
         public Guid DealerId { get; set; }
+        public string DealerName { get; set; } = null!;
         public string Period { get; set; } = null!;
         public decimal TargetAmount { get; set; }
         public decimal AchievedAmount { get; set; }
