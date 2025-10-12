b4a9ec9c154d95658192a4e45fb86046e9a6c4a2
LeHuyVuu <lehuyvuok@gmail.com>
2025-10-12 16:35:08 +0700

    [update] remove http conflict port, add 2 test controller for each of services

---
+++ OrderService/TestController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IntelliAIService;
+[ApiController]
+[Route("[controller]")]
+public class TestController
+{
+    [HttpGet]
+    public string Get()
+    {
+        return "Hello World!";
+    }
+}
